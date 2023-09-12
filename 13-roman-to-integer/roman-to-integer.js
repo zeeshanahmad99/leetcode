59607e53923462fc407550ddd493a4a62e4868df
Zeeshan Ahmad <zeems1234@gmail.com>
2023-09-12 20:39:54 +0100

    Time: 118 ms (40.90%) | Memory: 47.2 MB (55.23%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const symbols = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    let result = 0
+
+    /* 
+        1000
+        900
+        90
+        4
+     */
+
+    for(let i=0; i<s.length; i++) {
+        let value = symbols[s[i]]
+        
+        // edge case where if current value is less than next value
+        if(i+1 < s.length) {
+            const value2 = symbols[s[i+1]]
+            if(value2 > value) {
+                value = value2 - value
+                i++
+            }
+        } 
+        result += value
+    }
+
+    return result
+};
