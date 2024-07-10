b6a614e187850b7c5d4a5e730b1296e3f368a1dd
Zeeshan Ahmad <zeems1234@gmail.com>
2024-07-10 23:38:00 +0500

    Time: 60 ms (63.08%) | Memory: 53.6 MB (13.29%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,17 +3,31 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const halfLength = Math.floor(nums.length / 2)
-    const cache = new Map()
-    let majority = [nums[0], 1]
+    let res = nums[0], majority = 0
     for(let num of nums) {
-        if(!cache.has(num)) {
-            cache.set(num, 1)
-        } else {
-            const val = cache.get(num) + 1
-            cache.set(num, val)
-            if(val > majority[1]) majority = [num, val]
+        if(res === num) majority++
+        else majority--
+        if(majority === 0) {
+            res = num
+            majority = 1
         }
     }
-    return majority[0]
+
+    return res
+    
+    
+    
+    // const halfLength = Math.floor(nums.length / 2)
+    // const cache = new Map()
+    // let majority = [nums[0], 1]
+    // for(let num of nums) {
+    //     if(!cache.has(num)) {
+    //         cache.set(num, 1)
+    //     } else {
+    //         const val = cache.get(num) + 1
+    //         cache.set(num, val)
+    //         if(val > majority[1]) majority = [num, val]
+    //     }
+    // }
+    // return majority[0]
 };
