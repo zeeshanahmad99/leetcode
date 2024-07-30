0b2ae32d8171f0dd5329935f05f24fe9a30131e6
Zeeshan Ahmad <zeems1234@gmail.com>
2024-07-30 13:59:57 +0500

    Time: 83 ms (46.34%) | Memory: 61.4 MB (64.95%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,11 +3,10 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort((a, b) => a - b)
-    for(let i=0; i<nums.length - 1; i++) {
-        if(nums[i] === nums[i + 1]) {
-            return true
-        }
+    const cache = {}
+    for(let val of nums) {
+        if(cache[val]) return true
+        cache[val] = true
     }
     return false
 };
