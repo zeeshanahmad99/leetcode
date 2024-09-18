8a2029b11c5bd6b5715b2cca5ab6edcbb09debc3
Zeeshan Ahmad <zeems1234@gmail.com>
2024-09-18 21:39:56 +0500

    Time: 598 ms (95.94%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,5 +1,14 @@
+-- SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+-- FROM Prices p LEFT JOIN UnitsSold u
+-- ON p.product_id = u.product_id AND
+-- u.purchase_date BETWEEN start_date AND end_date
+-- group by product_id
+
+
 SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
-FROM Prices p LEFT JOIN UnitsSold u
-ON p.product_id = u.product_id AND
-u.purchase_date BETWEEN start_date AND end_date
-group by product_id
+from prices p 
+left join unitssold u on 
+        p.product_id = u.product_id and 
+        u.purchase_date between start_date and end_date
+group by p.product_id;
+
